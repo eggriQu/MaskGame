f3ada81cdcf4267748e56f5580d4f74b982bb24b
up2273222 <up2273222@myport.ac.uk>
2026-02-14 15:11:21 +0000

    Add follow enemy
    -Add test scene
    -Implement follow enemy V1

---
+++ Assets/Scripts/Enemies/Enemy_Follow/Enemy_FollowAI.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class Enemy_FollowAI : MonoBehaviour
+{
+    private AnimationCurve curveX;
+    private AnimationCurve curveY;
+    
+    private bool isFollowing;
+
+    private float currentTime;
+
+    [SerializeField] private float timeUntilSpawn;
+    [SerializeField] GameObject player;
+
+    private void Awake()
+    {
+        curveX = new AnimationCurve();
+        curveY = new AnimationCurve();
+        isFollowing = false;
+        this.gameObject.transform.position = player.transform.position;
+    }
+    private void FixedUpdate()
+    {
+        curveX.AddKey(Time.time, player.transform.position.x);
+        curveY.AddKey(Time.time ,player.transform.position.y);
+
+        if (isFollowing)
+        {
+            this.gameObject.transform.position = new Vector3(curveX.Evaluate(currentTime), curveY.Evaluate(currentTime), 0);
+        }
+    }
+    private void Update()
+    {
+        timeUntilSpawn -= Time.deltaTime;
+        if (timeUntilSpawn <= 0)
+        {
+            isFollowing = true;
+            currentTime += Time.deltaTime;
+        }
+    }
+}
